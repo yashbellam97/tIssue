149b0e0e8bfbbe5d34df33c813f89a0a374111f5
yashbellam97 <yashbellam97@gmail.com>
2021-01-03 15:24:22 -0500

    Add Project model

---
+++ backend/models/Project.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const projectSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    visibility: {
+        type: String,
+        default: "public",
+        enum: ["public", "private"]
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    admins: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model("Project", projectSchema);
