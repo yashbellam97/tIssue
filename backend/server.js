e0f27421fdb3530927b649b62eddcb2e8bd96dea
yashbellam97 <yashbellam97@gmail.com>
2021-01-03 00:01:50 -0500

    Connect to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDB = require("./dbconfig");
 
-require("dotenv").config();
+connectDB()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`));
