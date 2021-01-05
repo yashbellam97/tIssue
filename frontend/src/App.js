69505f9bbcc839a0d3f6350ed34f4cd8c24e0867
yashbellam97 <yashbellam97@gmail.com>
2021-01-05 16:55:47 -0500

    Add Signup component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import "./App.scss"
 
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
     return (
         <div>
             <Header />
+            <Signup />
             <Login />
         </div>
     );
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -3,34 +3,36 @@ import React from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 
-function Login() {
+function Signup() {
     return (
         <div className="d-flex justify-content-center my-5">
             <Form>
-                <h2 className="text-center mb-5">Log in</h2>
-                <Form.Group controlId="loginEmail">
+                <h2 className="text-center mb-5">Sign up</h2>
+                <Form.Group controlId="signupEmail">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" placeholder="username@example.com" />
                 </Form.Group>
-                <Form.Group controlId="loginPassword">
+                <Form.Group controlId="signupPassword">
                     <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="••••••••" />
+                </Form.Group>
+                <Form.Group controlId="signupConfirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
                     <Form.Control type="password" placeholder="••••••••" />
                 </Form.Group>
                 <br />
                 <div className="text-center">
                     <Button variant="primary" type="submit">
-                        Log in
+                        Sign up
                     </Button>
                 </div>
                 <br />
                 <div className="text-center mt-3">
-                    <a href="">Don't have an account? Sign up!</a>
-                    <br />
-                    <a href="" className="text-danger d-inline-block mt-2">Forgot Password?</a>
+                    <a href="">Have an account? Log in!</a>
                 </div>
             </Form>
         </div>
     );
 }
 
-export default Login;
+export default Signup;
