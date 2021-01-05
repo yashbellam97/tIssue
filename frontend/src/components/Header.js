b1176f6926e222e5513cc46facb9ae4a9cbeb216
yashbellam97 <yashbellam97@gmail.com>
2021-01-05 17:49:24 -0500

    Add react router navigation links to buttons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -7,18 +8,18 @@ import Button from 'react-bootstrap/Button';
 function Header() {
     return (
         <Navbar bg="dark" variant="dark" expand="md">
-            <Navbar.Brand href="/">tIssue</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">tIssue</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
                 </Nav>
                 <Nav variant="pills" className="justify-content-end">
                     <Nav.Item>
-                        <Button variant="secondary" className="my-1 mr-1">Log in</Button>
+                        <Button as={Link} to="/login" variant="secondary" className="my-1 mr-1">Log in</Button>
                     </Nav.Item>
                     <Nav.Item>
-                        <Button variant="primary" className="my-1">Sign up</Button>
+                        <Button as={Link} to="/signup" variant="primary" className="my-1">Sign up</Button>
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
