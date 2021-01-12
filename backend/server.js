a7c8c33fc766d01d9e4e62c81a085949dc1cfd8d
yashbellam97 <yashbellam97@gmail.com>
2021-01-12 16:23:54 -0500

    Add cors middleware to server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./dbconfig");
 
@@ -7,6 +8,7 @@ connectDB()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", require("./routes/api/users"));
