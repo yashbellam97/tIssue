55afbb0bd32fcfb6a8da86aed21dc2faecd2c36a
yashbellam97 <yashbellam97@gmail.com>
2021-01-12 20:12:15 -0500

    Add validation schema for Yup

---
+++ frontend/src/validationSchema.js
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+    username: Yup.string()
+        .min(5, "Must be at least 5 characters")
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+    firstName: Yup.string()
+        .max(20, "Must be 20 characters or less"),
+    lastName: Yup.string()
+        .max(20, "Must be 20 characters or less"),
+    password: Yup.string()
+        .min(8, "Must be at least 8 characters")
+        .required("Required"),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Password and Confirm must match")
+        .min(8, "Must be at least 8 characters")
+        .required("Required")
+});
+
+export default validationSchema;
